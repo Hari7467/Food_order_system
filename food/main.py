73f9c8e9ee4ccfd3a40c73fe8a4919027f598aec
Hari7467 <harivijayan2004@gmail.com>
2025-05-11 19:17:28 +0530

    Update main.py

--- food/main.py
+++ food/main.py
@@ -77,4 +77,6 @@ def confirmation():
     return render_template('confirmation.html', order=order_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
